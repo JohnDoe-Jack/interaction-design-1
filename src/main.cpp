5adc967aa5af66ce834d31a85f334985af803425
shuneish <grothendieck57@icloud.com>
2025-04-22 11:43:00 +0900

    soundComp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,39 @@
 //// filepath:
 ////home/iwasakim/Documents/PlatformIO/Projects/interaction-design-1/src/main.cpp
 #include "imu.hpp"
+#include "sound.hpp"
+#include "lgfx/v1/platforms/esp32/common.hpp"
 #include "slot_machine.hpp"
 #include <M5Unified.h>
+#include <WiFi.h>
+#include "AudioFileSourceSD.h"
+#include <cstdlib>
+
+#define TFCARD_CS_PIN 4 // SDカードのCSピン 
 
 static IMU myIMU;
-static SlotMachine slot(myIMU);
+static Sound sound; // Sound クラスのインスタンスを作成 (これは前回修正済み)
+static SlotMachine slot(myIMU,sound);
+
 
 void setup() { 
-  Serial.begin(115200);
-  delay(1000);
-
   auto cfg = M5.config();
-  cfg.serial_baudrate = 115200;
   M5.begin(cfg);
-  delay(1000);
-  M5.Speaker.setVolume(255);
+
+  Serial.begin(115200);
+  WiFi.mode(WIFI_OFF);  // Wi-Fiは使わないのでオフにしておく
+
+  if (!SD.begin(TFCARD_CS_PIN)) {
+    Serial.println("SD card mount failed!");
+    M5.Display.println("SD card mount failed!");
+    while (1) delay(1000);
+  }
+
+  // sound.begin(); 
   slot.begin(); 
 }
 
-void loop() { slot.update(); }
+void loop() {
+  // sound.loop(); 
+  slot.update();
+}
